73019d61e5cede6ed25fbb9ca9d46c784fc8c492
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-02-14 13:08:57 +0900

    .

---
+++ EL_CUSTOMTAG_JSTL/WebContent/custom_tag.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="simple" uri="http://www.itwill.co.kr/jsp/simpleTag" %>    
+<%
+	session.setAttribute("sUserId", "guard");
+	request.setAttribute("id","xxxx");
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+<simple:hello/>
+</body>
+</html>
--- EL_CUSTOMTAG_JSTL/src/com/itwill/simpleTag/HelloTag.java
+++ EL_CUSTOMTAG_JSTL/src/com/itwill/simpleTag/HelloTag.java
@@ -1,7 +1,13 @@
 package com.itwill.simpleTag;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
+
+import netscape.javascript.JSException;
 
 public class HelloTag  extends TagSupport{
 	public HelloTag() {
@@ -10,12 +16,29 @@ public class HelloTag  extends TagSupport{
 	@Override
 	public int doStartTag() throws JspException {
 		System.out.println("2.doStartTag()");
-		return super.doStartTag();
+		JspWriter out = pageContext.getOut();
+		HttpSession session=pageContext.getSession();
+		String sUserId = (String)session.getAttribute("sUserId");
+		if(sUserId==null) {
+			sUserId="GUEST";
+		}
+		try {
+			out.print(sUserId+"님 안뇽하세요!!!<br>");
+		} catch (IOException e) {
+			throw new JSException(e.getMessage());
+		}
+		return SKIP_BODY;
 	}
 	@Override
 	public int doEndTag() throws JspException {
 		// TODO Auto-generated method stub
 		System.out.println("2.doEndTag()");
-		return super.doEndTag();
+		JspWriter out = pageContext.getOut();
+		try {
+			out.print("<hr>");
+		} catch (IOException e) {
+			throw new JSException(e.getMessage());
+		}
+		return EVAL_PAGE;
 	}
 }
