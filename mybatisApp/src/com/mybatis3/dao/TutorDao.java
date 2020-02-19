42395c6db2fcc705f14bb2dc1228adf59162aa74
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-02-19 15:43:00 +0900

    .

--- mybatisApp/src/com/mybatis3/dao/TutorDao.java
+++ mybatisApp/src/com/mybatis3/dao/TutorDao.java
@@ -1,8 +1,66 @@
 package com.mybatis3.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.mybatis3.domain.Tutor;
+
 public class TutorDao 
 {
+	private SqlSessionFactory sqlSessionFactory;
+	public static final String NAMESPACE=
+			"com.mybatis3.mapper.TutorMapper.";
+	public TutorDao() {
+		InputStream mybatisConfigInputStream=null;
+		try {
+			mybatisConfigInputStream = 
+					Resources.getResourceAsStream("mybatis-config.xml");
+			this.sqlSessionFactory =
+					new SqlSessionFactoryBuilder()
+					.build(mybatisConfigInputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	/*
+	 JOIN(Tutor + Courses + Address)
+	 */
+	public Tutor findTutorWithCoursesAndAddressById(Integer tutorId) {
+		SqlSession sqlSession=
+				sqlSessionFactory.openSession(true);
+		Tutor tutor=sqlSession.selectOne(
+				NAMESPACE+"findTutorWithCoursesAndAddressById", tutorId);
+		sqlSession.close();
+		return tutor;
+	}
+	/*
+	 JOIN(Tutot + Courses )
+	 */
+	public Tutor findTutorWithCoursesById(Integer tutorId) {
+		SqlSession sqlSession=
+				sqlSessionFactory.openSession(true);
+		Tutor tutor=sqlSession.selectOne(
+				NAMESPACE+"findTutorWithCoursesById", tutorId);
+		sqlSession.close();
+		return tutor;
+	}
+	/*
+	Tutor
+	*/
+	public Tutor findTutorById(Integer tutorId) {
+		SqlSession sqlSession=
+				sqlSessionFactory.openSession(true);
+		Tutor tutor=sqlSession.selectOne(
+				NAMESPACE+"findTutorById", tutorId);
+		sqlSession.close();
+		return tutor;
+	}
 	
 }
 
--- mybatisApp/src/com/mybatis3/dao/TutorDaoMain.java
+++ mybatisApp/src/com/mybatis3/dao/TutorDaoMain.java
@@ -1,13 +1,41 @@
 package com.mybatis3.dao;
 
+import java.util.List;
+
+import com.mybatis3.domain.Address;
+import com.mybatis3.domain.Course;
+import com.mybatis3.domain.Tutor;
+
 public class TutorDaoMain {
 
 	public static void main(String[] args) {
+		TutorDao tutorDao=new TutorDao();
 		System.out.println("----------findTutorById--------");
-
-		System.out.println("----------selectTutorWithAddressAndCourses--------");
-
-		System.out.println("-------------nestedSelectTutorById----------------");
+		System.out.println("### "+tutorDao.findTutorById(1));
+		System.out.println("----------selectTutorWithCourses--------");
+		System.out.println("### "+tutorDao.findTutorWithCoursesById(1));
+		
+		Tutor tutor1 = tutorDao.findTutorWithCoursesById(1);
+		List<Course> courseList1 = tutor1.getCourses();
+		System.out.println("Tutor1:"+tutor1);
+		for (Course course : courseList1) {
+			System.out.println("\t"+tutor1.getName() +"강사코스 -->"+
+					course.toString());
+			
+		}
+		System.out.println("----------findTutorWithCoursesAndAddressById--------");
+		Tutor tutor2 = tutorDao.findTutorWithCoursesAndAddressById(2);
+		Address address2=tutor2.getAddress();
+		List<Course> courseList2 = tutor2.getCourses();
+		
+		System.out.println("Tutor2:"+tutor2);
+		System.out.println("\t"+tutor2.getName()+"강사 주소-->"+address2);
+		for (Course course : courseList2) {
+			System.out.println("\t"+tutor2.getName() +"강사 코스 -->"+
+					course.toString());
+			
+		}
+		//System.out.println("-------------nestedSelectTutorById----------------");
 
 	}
 
