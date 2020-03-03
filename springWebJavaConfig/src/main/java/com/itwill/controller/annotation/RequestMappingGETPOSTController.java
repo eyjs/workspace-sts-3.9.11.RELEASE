5d135d6af4211b478f7736ce50e3ff9c4dfb722f
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-03-03 19:36:32 +0900

    ..

---
+++ springWebJavaConfig/src/main/java/com/itwill/controller/annotation/RequestMappingGETPOSTController.java
@@ -0,0 +1,27 @@
+package com.itwill.controller.annotation;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class RequestMappingGETPOSTController {
+	@RequestMapping(value = "/login.do",method = RequestMethod.GET)
+	public String login_form_get() {
+		return "/WEB-INF/views/get_login_form.jsp";
+	}
+	@RequestMapping(value = "/login.do",method = RequestMethod.POST)
+	public String login_action_post(@RequestParam(value = "id",required = true,defaultValue = "") String id,
+									@RequestParam String password,
+									HttpSession session) {
+		System.out.println("### id parameter:"+id);
+		System.out.println("### password parameter:"+password);
+		boolean isLogin=true;
+		if(isLogin) {
+			session.setAttribute("sUserId", id);
+		}
+		return "/WEB-INF/views/post_login_result.jsp";
+	}
+}
