59e6785f6239ba7a3be13943f83f5f417bb72824
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-03-08 15:03:30 +0900

    ..

--- guest_spring_controller/src/main/java/com/itwill/guest/GuestDaoImplMyBatis.java
+++ guest_spring_controller/src/main/java/com/itwill/guest/GuestDaoImplMyBatis.java
@@ -1,88 +1,64 @@
 package com.itwill.guest;
 
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
-
-public class GuestDaoImplMybatis  implements GuestDao{
-
-	private SqlSessionFactory sqlSessionFactory;
+public class GuestDaoImplMyBatis  implements GuestDao{
+	private SqlSession sqlSession;
 	public final static String NAMESPACE=
 			"com.itwill.guest.mapper.GuestMapper.";
-	
-	public GuestDaoImplMybatis() throws Exception {
-		InputStream in=null;
-		try {
-			in=Resources.getResourceAsStream("mybatis-config.xml");
-			this.sqlSessionFactory = 
-					new SqlSessionFactoryBuilder()
-					.build(in);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
+	public GuestDaoImplMyBatis() throws Exception {
 	}
-
+	public GuestDaoImplMyBatis(SqlSession sqlSession) throws Exception {
+		this.sqlSession=sqlSession;
+	}
+	public SqlSession getSqlSession() {
+		return sqlSession;
+	}
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
 	/*
 	 * READ ALL
 	 */
 	public List<Guest> selectAll() throws Exception {
 		List<Guest> guestList = new ArrayList<Guest>();
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		guestList = sqlSession.selectList(NAMESPACE+"selectAll");
-		sqlSession.close();
 		return guestList;
 	}
-	
 	/*
 	 * CREATE
 	 */
 	public boolean insertGuest(Guest guest) throws Exception {
 		boolean isSuccess = false;
-		SqlSession sqlSession=sqlSessionFactory.openSession(true);
 		int insertRowCount=sqlSession.insert(NAMESPACE+"insertGuest", guest); 
 		if(insertRowCount==1) {
 			isSuccess=true;
 		}else {
 			isSuccess=false;
 		}
-		sqlSession.commit();
-		sqlSession.close();
 		return isSuccess;
 	}
-
 	/*
 	 * READ ONE
 	 */
 	public Guest selectByNo(int no) throws Exception {
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		Guest guest =sqlSession.selectOne(NAMESPACE+"selectByNo",no);
-		sqlSession.close();
 		return guest;
 	}
-	
-	
-	
 	/*
 	 * DELETE
 	 */
 	public boolean deleteGuest(int guest_no) throws Exception{
 		boolean deleteOK=false;
-		SqlSession sqlSession=sqlSessionFactory.openSession(true);
 		int deleteRowCount=sqlSession.insert(NAMESPACE+"deleteGuest", guest_no); 
 		if(deleteRowCount==1) {
 			deleteOK=true;
 		}else {
 			deleteOK=false;
 		}
-		sqlSession.commit();
-		sqlSession.close();
 		return deleteOK;
 	}
 	/*
@@ -90,15 +66,12 @@ public class GuestDaoImplMybatis  implements GuestDao{
 	 */
 	public boolean updateGuest(Guest updateGuest) throws Exception{
 		boolean updateOK=false;
-		SqlSession sqlSession=sqlSessionFactory.openSession(true);
 		int updateRowCount=sqlSession.insert(NAMESPACE+"updateGuest", updateGuest); 
 		if(updateRowCount==1) {
 			updateOK=true;
 		}else {
 			updateOK=false;
 		}
-		sqlSession.commit();
-		sqlSession.close();
 		return updateOK;
 	}
 	
