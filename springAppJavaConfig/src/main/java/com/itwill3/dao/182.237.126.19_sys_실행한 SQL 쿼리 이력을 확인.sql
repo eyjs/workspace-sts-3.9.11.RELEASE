f1667ed15253505a2c4b96cad3e8a9c343eeb30c
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-03-08 22:17:40 +0900

    .

--- springAppJavaConfig/src/main/java/com/itwill3/dao/182.237.126.19_sys_실행한 SQL 쿼리 이력을 확인.sql
+++ springAppJavaConfig/src/main/java/com/itwill3/dao/182.237.126.19_sys_실행한 SQL 쿼리 이력을 확인.sql
@@ -14,3 +14,24 @@ WHERE
   AND parsing_schema_name <> 'EXFSYS'
 ORDER BY 
   last_active_time DESC;
+
+desc X$DBGALERTEXT;
+
+select originating_timestamp, message_text ,s.*
+from X$DBGALERTEXT s
+where originating_timestamp > sysdate - 7;
+    
+select * from V$SQL
+         order by first_load_time desc;    
+DESC V$SQL; 
+      
+      
+select T1.hash_value, T1.first_load_time, T1.module, T1.sql_fulltext, T1.last_load_time,T1.*
+  from (select *
+          from V$SQL
+         order by first_load_time desc) T1
+ where parsing_schema_name like '%'
+   and upper(sql_fulltext) not like upper('%V$SQL%')  --자기자신은 제외
+   and sql_fulltext like '%'
+ order by last_active_time desc
+  
