42c4f63c7ec25a5d8225eeed3e9f9667e9efe883
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-03-10 13:07:59 +0900

    .

--- springWebJavaConfig/src/main/java/com/itwill/config/WebApplicationInitializerConfig.java
+++ springWebJavaConfig/src/main/java/com/itwill/config/WebApplicationInitializerConfig.java
@@ -10,7 +10,63 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
+/*
+web.xml을 대신할 java 클래스
+<?xml version="1.0" encoding="UTF-8"?>
+<web-app version="2.5" xmlns="http://java.sun.com/xml/ns/javaee"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://java.sun.com/xml/ns/javaee https://java.sun.com/xml/ns/javaee/web-app_2_5.xsd">
 
+	<filter>
+		<filter-name>encodingFilter</filter-name>
+		<filter-class>org.springframework.web.filter.CharacterEncodingFilter
+		</filter-class>
+		<init-param>
+			<param-name>encoding</param-name>
+			<param-value>UTF-8</param-value>
+		</init-param>
+		<init-param>
+			<param-name>forceEncoding</param-name>
+			<param-value>true</param-value>
+		</init-param>
+	</filter>
+	<filter-mapping>
+		<filter-name>encodingFilter</filter-name>
+		<url-pattern>/*</url-pattern>
+	</filter-mapping>
+
+	<!-- The definition of the Root Spring Container shared by all Servlets 
+		and Filters -->
+	<context-param>
+		<param-name>contextConfigLocation</param-name>
+		<param-value>classpath:spring/application-config.xml</param-value>
+	</context-param>
+	<!-- Creates the Spring Container shared by all Servlets and Filters -->
+	<listener>
+		<listener-class>org.springframework.web.context.ContextLoaderListener</listener-class>
+	</listener>
+	<!-- Processes application requests -->
+	<servlet>
+		<servlet-name>appServlet</servlet-name>
+		<servlet-class>org.springframework.web.servlet.DispatcherServlet
+		</servlet-class>
+		<init-param>
+			<param-name>contextConfigLocation</param-name>
+			<!-- <param-value>classpath:spring/mvc-config.xml</param-value> -->
+			<param-value>classpath:spring/mvc-config-view-resolver.xml
+			</param-value>
+		</init-param>
+		<load-on-startup>1</load-on-startup>
+	</servlet>
+
+	<servlet-mapping>
+		<servlet-name>appServlet</servlet-name>
+		<url-pattern>*.do</url-pattern>
+	</servlet-mapping>
+
+</web-app>
+
+*/
 public class WebApplicationInitializerConfig implements WebApplicationInitializer {
 	
 	public void onStartup(ServletContext servletContext) throws ServletException {
@@ -22,7 +78,7 @@ public class WebApplicationInitializerConfig implements WebApplicationInitialize
 
 		AnnotationConfigWebApplicationContext servletAppContext = new AnnotationConfigWebApplicationContext();
 		servletAppContext.register(ServletConfig.class);
-		servletAppContext.register(ControllerConfig.class);
+		//servletAppContext.register(ControllerConfig.class);
 
 		DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
 
