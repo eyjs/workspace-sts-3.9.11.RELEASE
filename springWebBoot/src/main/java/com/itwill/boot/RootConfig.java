e0d04136ce6e12384363f714585f16c00c98e5e2
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-03-17 08:17:33 +0900

    .

---
+++ springWebBoot/src/main/java/com/itwill/boot/RootConfig.java
@@ -0,0 +1,29 @@
+package com.itwill.boot;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/*
+application-config.xml을 대신할 java 클래스
+ */
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import com.itwill.service.UserService;
+@Configuration
+public class RootConfig {
+	/*
+	@Bean
+    public UserService userService() {
+    	UserService userService = new UserService();
+    	userService.setMessageSource(messageSource());
+    	return userService;
+    }
+    */
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages/guest","messages/messages","messages/user");
+		return messageSource;
+	}
+	
+}
