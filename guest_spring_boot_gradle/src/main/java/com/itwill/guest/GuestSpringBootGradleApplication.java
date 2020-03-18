2d57e2ec4d868024cfd1a43cd7ad1b25666736d7
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-03-18 13:40:21 +0900

    .

---
+++ guest_spring_boot_gradle/src/main/java/com/itwill/guest/GuestSpringBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.itwill.guest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestSpringBootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestSpringBootGradleApplication.class, args);
+	}
+
+}
